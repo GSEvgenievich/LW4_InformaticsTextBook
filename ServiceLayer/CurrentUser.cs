a3170967a2b8571c5337155fb2de873cbdfdc96b
Den4ikProgrammer <den4ik5080@yandex.ru>
2025-09-21 18:17:41 +0300

    Добавлен статический класс для хранения данных текущего пользователя

---
+++ ServiceLayer/CurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using ServiceLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public static class CurrentUser
+    {
+        public static int UserID { get; set; }
+        public static string UserLogin { get; set; }
+        public static string UserPassword { get; set; }
+    }
+}
