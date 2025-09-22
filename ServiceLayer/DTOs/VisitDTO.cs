7169569fb8305fa3f83cced5b100d9a35c0d1982
Den4ikProgrammer <den4ik5080@yandex.ru>
2025-09-22 16:40:24 +0300

    Добавление DTOs: +QuestionDTO +VisitDTO

---
+++ ServiceLayer/DTOs/VisitDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.DTOs
+{
+    public class VisitDTO
+    {
+        public string LectionName { get; set; }
+        public int VisitTimeSeconds { get; set; }
+
+        public string DisplayTime
+        {
+            get
+            {
+                var ts = TimeSpan.FromSeconds(VisitTimeSeconds);
+                if (ts.TotalMinutes >= 1)
+                    return $"{ts.Minutes} мин {ts.Seconds} сек";
+                else
+                    return $"{ts.Seconds} сек";
+            }
+        }
+    }
+}
